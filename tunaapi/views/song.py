7cbeebfc02c932bbdbc96afa96af0892b57a3f51
Erin Stephens <erebstephens@gmail.com>
2023-06-06 19:34:58 -0500

    basic CRUD functions on song requests

--- tunaapi/views/song.py
+++ tunaapi/views/song.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from tunaapi.models import Song
+from tunaapi.models import Song, Artist
 
 class SongView(ViewSet):
   
@@ -20,6 +20,38 @@ class SongView(ViewSet):
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
         return Response(serializer.data)
+      
+    def create(self, request):
+      
+        artist = Artist.objects.get(pk=request.data["artist"])
+        
+        song = Song.objects.create(
+          title=request.data["title"],
+          artist=artist,
+          album=request.data["album"],
+          length=request.data["length"],
+        )
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+      
+    def update(self, request, pk):
+      
+        song = Song.objects.get(pk=pk)
+        song.title = request.data["title"]
+        song.album = request.data["album"]
+        song.length = request.data["length"]
+        
+        artist = Artist.objects.get(pk=request.data["artist"])
+        song.artist = artist
+        song.save()
+        
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+      
+    def destroy(self, request, pk):
+      
+        song = Song.objects.get(pk=pk)
+        song.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
